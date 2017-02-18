54d7f78feef7dd567fff78c780db0b2636b782f6
LaurenceRouge <laurence.rouge@hetic.net>
2017-02-18 20:08:08 +0100

    update[Home] : dynamic data on news
    - DefaultController : get data
    - index.html.twig : display + filter data in foreach loop

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Episode;
 
 class DefaultController extends Controller
 {
@@ -13,9 +14,21 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        // On récupère le repository
+        $repository = $this->getDoctrine()
+            ->getManager()
+            ->getRepository('AppBundle:News')
+        ;
+
+        // On récupère l'entité correspondante à l'id $id
+        $news = $repository->findAll();
+
+
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'news' => $news,
         ]);
     }
 }

e605436a29f762e3307c3ed025e806414752041f
LaurenceRouge <laurence.rouge@hetic.net>
2017-02-19 01:39:46 +0100

    add[FrontNewsList] : create new page for news
    - Controller + view + route
    - dynamic data from database

---
+++ src/AppBundle/Controller/FrontNewsListController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: laurence
+ * Date: 19/02/2017
+ * Time: 01:29
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\News;
+
+
+/**
+ * @Route("news")
+ */
+class FrontNewsListController extends Controller
+{
+    /**
+     * @Route("/", name="news")
+     */
+
+    public function newsAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $news = $em->getRepository('AppBundle:News')->findAll();
+
+
+        return $this->render('news/news.html.twig', array(
+            'news' => $news,
+        ));
+    }
+}

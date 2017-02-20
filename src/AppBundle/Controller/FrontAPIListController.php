20d022c0b949970a6b052b8fa5fcd03369727239
LaurenceRouge <laurence.rouge@hetic.net>
2017-02-20 11:22:37 +0100

    add[Characters page + api connection]

---
+++ src/AppBundle/Controller/FrontAPIListController.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: laurence
+ * Date: 19/02/2017
+ * Time: 01:29
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\News;
+
+
+/**
+ * @Route("univers")
+ */
+class FrontAPIListController extends Controller
+{
+    /**
+     * @Route("/character", name="character")
+     */
+
+    public function newsAction()
+    {
+        $opts = array(
+            'http'=>array(
+                'method'=>"GET",
+                'header'=>"Accept: application/json\r\n"
+            )
+        );
+
+        $context = stream_context_create($opts);
+
+        // Accès à un fichier HTTP avec les entêtes HTTP indiqués ci-dessus
+        $temp = file_get_contents('http://127.0.0.1:8000/api/characters', false, $context);
+        $api = json_decode($temp);
+
+        return $this->render('univers/apilist.html.twig', array(
+            'apis' => $api,
+        ));
+    }
+}
